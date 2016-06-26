d134b670689f06e59d81ce832b6e8be39095552c
digital-mac <ajdasoft@gmail.com>
2016-06-26 18:14:20 +0200

    Initial

--- YiiTwilio.php
+++ YiiTwilio.php
@@ -8,23 +8,25 @@ use Twilio\Services\Twilio;
 class YiiTwilio extends Component
 {
 
-
-
     /**
-     * The internal Adldap object.
+     * The internal Twilio object.
      *
-     * @var object Adldap
+     * @var object Twilio
      */
     private $twilioClass;
 
 
     /**
-     * Options variable for the Twilio module.
-     * See Twilio __construct function for possible values.
-     *
-     * @var array Array with option values
+	 ** @var string $account_sid -> Twilio Account ID
      */
-    public $options = [];
+    public $account_sid;
+
+
+    /**
+	 ** @var string $auth_key -> Twilio Authorization Key
+     */
+
+    public $auth_key;
 
 
 
@@ -36,14 +38,33 @@ class YiiTwilio extends Component
     public function init()
     {
        
-        try {
-            $this->twilioClass = new Services_Twilio($this->options);
-        } catch (Exception $e) {
+      try {
+            $this->twilioClass = new Services_Twilio($this->account_sid, $this->auth_key);
+      } catch (Exception $e) {
             throw $e;
-        }
+      }
 
        
     }
+
+    /**
+     * Use magic PHP function __call to route function calls to the Twilio class.
+     * Look into the Twilio class for possible functions.
+     *
+     * @param string $methodName Method name from Twilio class
+     * @param array $methodParams Parameters pass to method
+     * @return mixed
+     */
+
+    public function __call($methodName, $methodParams)
+    {
+        if (method_exists($this->twilioClass, $methodName)) {
+            return call_user_func_array(array($this->twilioClass, $methodName), $methodParams);
+        } else {
+            return parent::__call($methodName, $methodParams);
+        }
+    }
+
 
 
 
